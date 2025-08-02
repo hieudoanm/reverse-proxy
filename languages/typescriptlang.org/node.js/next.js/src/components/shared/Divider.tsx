43ef988190b1ecf8e6f852c4e25614e8b7d8a4e4
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-08-02 08:51:09 +0700

    remove next.js

--- languages/typescriptlang.org/node.js/next.js/src/components/shared/Divider.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-
-export const Divider: FC = () => {
-  return <div className="border-t border-neutral-800" />;
-};
--- languages/typescriptlang.org/node.js/next.js/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Landing } from '@reverse-proxy/components/landing/Landing';
-import { NextPage } from 'next';
-
-const HomePage: NextPage = () => {
-  return <Landing />;
-};
-
-export default HomePage;
