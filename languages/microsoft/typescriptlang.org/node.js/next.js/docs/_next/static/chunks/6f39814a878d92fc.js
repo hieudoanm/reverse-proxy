13031d4aa3ed4954c34253a110912a9d8474ee54
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-10-25 12:32:34 +0700

    fix next.js

--- languages/microsoft/typescriptlang.org/node.js/next.js/docs/_next/static/chunks/6f39814a878d92fc.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__('/', [
-  'static/chunks/b7dd314957e78d45.js',
-  'static/chunks/4e73f082abcc4867.js',
-  'static/chunks/e47d710ee4d6cccf.js',
-  'static/chunks/turbopack-8757ac059e4bb1cd.js',
-]);
